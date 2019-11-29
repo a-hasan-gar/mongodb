8b1a46b9f1b8483094e723db1d1e01722ec72072
ilmi <nurhikmahilmi94@gmail.com>
2019-11-29 21:46:12 +0700

    views.py, urls.py

--- nearby_mongodb/urls.py
+++ nearby_mongodb/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+import search_nearby.urls as search_nearby
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^search/', include(search_nearby)),
 ]
