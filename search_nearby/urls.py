3e2b426e4c2e1402d40b9bf2ecb2947fe9c6b1a1
Ahmad Hasan Siregar <a.hasan.gar@gmail.com>
2019-12-02 20:18:01 +0700

    Add function to handle nearby_accidents

--- search_nearby/urls.py
+++ search_nearby/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('accidents/nearby', nearby_accidents, name='nearby-accidents'),
 ]
--- search_nearby/views.py
+++ search_nearby/views.py
@@ -1,16 +1,64 @@
+import pprint
+from enum import Enum
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.conf import settings
-import pprint
 from pymongo import MongoClient, GEO2D, GEOSPHERE
 
 
-# Create your views here.
 def index(request):
     return render(request, 'search_nearby/home.html')
 
 # Connect with mongoDB
 collection_accident = settings.DB.accident
 collection_accident.create_index([("Location", GEO2D)])
+
+def nearby_accidents(request):
+    lat = request.GET.get('lat', None)
+    lon = request.GET.get('lon', None)
+
+    if request.method != 'GET' or not lat or not lon:
+        return JsonResponse({})
+
+    radius = request.GET.get('radius', 500)
+    limit = request.GET.get('limit', 0)
+
+    accidents = get_nearby_accidents(lat, lon, radius, limit)
+
+    filter_type = request.GET.get('filter_type', FilterType.LIGHT_CONDITIONS)
+    frequencies = transform_to_filter(accidents, filter_type)
+
+    return JsonResponse({frequencies})
+
+class FilterType(Enum):
+    LIGHT_CONDITIONS = 0,
+    ROAD_SURFACE_CONDITIONS = 1,
+    ROAD_TYPE = 2,
+    WEATHER_CONDITIONS = 3,
+    YEAR = 4,
+
+def transform_to_filter(accidents, filter_type):
+    frequencies = {}
+
+    for accident in accidents:
+        if filter_type == FilterType.LIGHT_CONDITIONS:
+            val = accident['Light_Conditions']
+        elif filter_type == FilterType.ROAD_SURFACE_CONDITIONS:
+            val = accident['Road_Surface_Conditions']
+        elif filter_type == FilterType.ROAD_TYPE:
+            val = accident['Road_Type']
+        elif filter_type == FilterType.WEATHER_CONDITIONS:
+            val = accident['Weather_Conditions']
+        elif filter_type == FilterType.YEAR:
+            val = accident['Year']
+
+        if val not in frequencies:
+            frequencies[val] = 1
+        else:
+            frequencies[val] += 1
+
+    return frequencies
 
 # Query
 def get_nearby_accident(lon, lat, radius=500, lim=0):
