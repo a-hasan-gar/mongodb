50b65163b5891fe76f4e93a89ad1b510bb2c4ab0
ilmi <nurhikmahilmi94@gmail.com>
2019-12-12 12:08:03 +0700

    search fixed

--- search_nearby/urls.py
+++ search_nearby/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('accidents/nearby', nearby_accidents, name='nearby-accidents'),
-
+    path('coordinate', page_coordinate, name='lot-lang'),
     path('accidents/nearbycoord', nearby_accidents_coord, name='nearby-accidents-coord'),
     # path('coordinate', page_coordinate, name='lot-lang'),
 
